6868e2169995c1489189c37a29edbc8c9e25a6b4
fanyuming <anson.fan@outlook.com>
2023-04-22 13:45:44 +1000

    feat: darkmode with user system preferences

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import * as React from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
 function App() {
-  return (<>
-  <h1>Hello world! 三七食行!</h1>
-  </>);
+
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
+  return (<ThemeProvider theme={theme}>
+    <CssBaseline />
+    <h1>Hello world! 三七食行!</h1>
+  </ThemeProvider>);
 }
 
 export default App;
