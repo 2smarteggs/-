c8ca0674d92f9e823afab7455de636faa108d8e3
fanyuming <anson.fan@outlook.com>
2023-04-23 20:21:29 +1000

    add: add lottie progress animation local json and update progress section layout with grid

--- src/components/Home/ProgressSection.jsx
+++ src/components/Home/ProgressSection.jsx
@@ -1,19 +1,25 @@
 import React, {  useRef } from 'react';
 import SELottiePlayer from "../Common/SELottiePlayer";
 import SEMuiFloatingActionButton from "../Common/SEMuiFloatingActionButton";
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import progressBar from "../../animations/progressBar.json";
 
 const ProgressSection = () => {
 
-    const playerRef = useRef();
+    const playerRefCarbon = useRef();
+    const playerRefProtein = useRef();
 
     const fabRef = useRef();
 
     const handleClick = (e) => {
-        playerRef.current.play(0.9);
+        playerRefCarbon.current.play(0.75);
+        playerRefProtein.current.play(0.85);
     }
 
     const handleClickB = (e) => {
-        playerRef.current.reset();
+        playerRefCarbon.current.reset();
+        playerRefProtein.current.reset();
     }
 
     const handleClickC = (e) => {
@@ -24,11 +30,31 @@ const ProgressSection = () => {
         <button onClick={handleClick}>aaa</button>
         <button onClick={handleClickB}>bbb</button>
         <button onClick={handleClickC}>ccc</button>
-        <SELottiePlayer
-            ref={playerRef}
-            src="https://assets3.lottiefiles.com/packages/lf20_UJNc2t.json"
-            style={{ height: '300px', width: '300px' }}
-        />
+
+        <Container maxWidth="sm">
+            <Grid container spacing={2}>
+                <Grid item xs={4}>
+                    碳水化合物
+                </Grid>
+                <Grid item xs={8}>
+                    <SELottiePlayer
+                        ref={playerRefCarbon}
+                        src={progressBar}
+                        style={{ height: '100%', width: '100%' }}
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    蛋白质
+                </Grid>
+                <Grid item xs={8}>
+                    <SELottiePlayer
+                        ref={playerRefProtein}
+                        src={progressBar}
+                        style={{ height: '100%', width: '100%' }}
+                    />
+                </Grid>
+            </Grid>
+        </Container>
         <SEMuiFloatingActionButton
             ref={fabRef}
         />
